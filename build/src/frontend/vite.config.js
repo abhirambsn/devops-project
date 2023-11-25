42c8bee4fe7b0333af2136fb5c6de0ef1a359c68
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2023-11-25 12:22:57 +0530

    Added kubeconfig support

--- build/src/frontend/vite.config.js
+++ build/src/frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
